ab6304a89a245dce37b22e386910243afa7c1b02
solem79 <154937992+solem79@users.noreply.github.com>
2026-03-26 03:42:37 +0100

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,21 @@
+function initCounters() {
+  const counters = document.querySelectorAll(".count");
+  if (!counters.length) return;
 
+  counters.forEach(el => {
+    let target = +el.dataset.target;
+    let start = 0;
+    let inc = target / 120;
+
+    function run() {
+      start += inc;
+      if (start < target) {
+        el.textContent = Math.floor(start) + "+";
+        requestAnimationFrame(run);
+      } else {
+        el.textContent = target + "+";
+      }
+    }
+    run();
+  });
+}
